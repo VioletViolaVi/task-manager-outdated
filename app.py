5f1e44ad2e64727f79c8a48b1e66037f049294d4
VioletViolaVi <vivian.v18723@gmail.com>
2020-07-29 17:44:04 +0000

    added gitignore file

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import os
 from flask import Flask
-
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+
+
+app.config["MONGO_DBNAME"] = "task_manager"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 @app.route("/")
